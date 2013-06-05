720824321de5a9516db007051cec584e5278ae54
johannchen <johannchen@gmail.com>
2013-06-05 14:53:47 -0700

    init yeoman angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('questionsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
