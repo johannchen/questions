a13cf2b288ff2ed770ba9758d2434455291f9007
Johann Chen <johannchen@gmail.com>
2013-07-16 16:13:38 -0700

    add/remove question

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -4,35 +4,43 @@ angular.module('questionsApp')
   .controller('AdminCtrl', function ($scope, $filter, $timeout, $location, angularFire, fbURL) {
     //$scope.pendingQuestions = Question.query({status:'pending'});
     //$scope.approvedQuestions = Question.query({status:'approved'});
+    $scope.category = "Light";
+    $scope.editedQuestion = null;
     angularFire(fbURL, $scope, 'questions', []).then(function() {
 
 //$scope.approvedQuestions = $filter("status")(questions, "approved");
  
-    $scope.approve = function(question) {
-      var index = $scope.pendingQuestions.indexOf(question);
-      question.status = 'approved';
-      $scope.approvedQuestions.unshift(question);
-      $scope.pendingQuestions.splice(index, 1);
-    };
-
-    $scope.submitQuestion = function() {
-      $scope.newQuestion.status = 'approved';
-      $scope.newQuestion.popularity = 1;
-      $scope.newQuestion.created_at = new Date();
-      
-      
-      $scope.newQuestion = null;
-    };
-
-    $scope.deleteQuestion = function(question) {
-      if(question.status === 'pending') {
+      $scope.approve = function(question) {
         var index = $scope.pendingQuestions.indexOf(question);
+        question.status = 'approved';
+        $scope.approvedQuestions.unshift(question);
         $scope.pendingQuestions.splice(index, 1);
-      } else {
-        var index = $scope.approvedQuestions.indexOf(question);
-        $scope.approvedQuestions.splice(index, 1);
-      }
-    };
+      };
+
+      $scope.addQuestion = function() {
+        if(!$scope.newQuestion.length) {
+          return;
+        }
+
+        $scope.questions.push( {
+          question: $scope.newQuestion,
+          category: $scope.category,
+          approved: true,
+          popularity: 1,
+          created_at: new Date(),
+        })
+        
+        $scope.newQuestion = null;
+      };
+
+      $scope.removeQuestion = function(question) {       
+        var index = $scope.questions.indexOf(question);
+        $scope.questions.splice(index, 1);
+      };
+
+      $scope.editQuestion = function(question) {
+        $scope.editedQuestion = question;
+      };
     });
     
     
